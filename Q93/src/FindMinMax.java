e45932b8f723e207ae89def8e52baed6c9a41d03
Adi Strasser <adistrasser350@gmail.com>
2024-06-24 20:53:33 +0300

    24/06/2024

--- Q93/src/FindMinMax.java
+++ Q93/src/FindMinMax.java
@@ -1,5 +1,40 @@
+// Import the Scanner.
+import java.util.Scanner;
+
 public class FindMinMax {
+    // Create a new Scanner object.
+    static Scanner input = new Scanner(System.in);
+
+    // Create a function that returns the difference between the smallest and the largest numbers in the string.
+    public static int minMaxDiff(String str) {
+        // Declaring the variables.
+        char small = str.charAt(0); // -> The smallest number in the string.
+        char big = str.charAt(0); // -> The largest number in the string.
+
+        // Finding the smallest and the biggest digits in the string.
+        for (int i = 0; i < str.length(); i++) {
+            if (small > str.charAt(i)) {
+                small = str.charAt(i);
+            }
+
+            if (big < str.charAt(i)) {
+                big = str.charAt(i);
+            }
+        }
+
+        // Returning the difference.
+        return big - small;
+    }
+
     public static void main(String[] args) {
-        // TODO Do this exercise.
+        // Declaring the variables.
+        String str; // -> A string as input from the user.
+
+        // Getting the input from the user -> A string.
+        System.out.print("Please enter digits: ");
+        str = input.nextLine();
+
+        // Printing the original input and the result of the difference between the biggest and smallest digits.
+        System.out.println("\nThe difference between the biggest and smallest digits in: " + str + " is " + minMaxDiff(str));
     }
 }
--- Q94/src/IsPrimeFunc.java
+++ Q94/src/IsPrimeFunc.java
@@ -1,5 +1,30 @@
+// Import the Scanner.
+import java.util.Scanner;
+
 public class IsPrimeFunc {
+    // Create a new Scanner object.
+    static Scanner input = new Scanner(System.in);
+
+    // Create a function that returns if  number is prime or not.
+    public static boolean isPrime(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        // TODO Do this exercise.
+        // Declare the variables.
+        int num; // -> A number as input from the user.
+
+        // Getting the input from the user -> A number.
+        System.out.print("Please enter a number: ");
+        num = input.nextInt();
+
+        // Printing the number and if it is prime or not.
+        System.out.println("\nThe number: " + num + " is " + (isPrime(num) ? "" : "not ") + "a prime number.");
     }
 }
